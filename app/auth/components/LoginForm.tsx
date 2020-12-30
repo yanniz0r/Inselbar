58eccdee2365289119b4f4a8f901340416796198
Yannic Inselmann <yannic.inselmann@gmail.com>
2020-12-30 23:51:12 +0100

    feat: prettier login

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -30,18 +30,28 @@ export const LoginForm = (props: LoginFormProps) => {
   }
 
   return (
-    <div>
-      <form onSubmit={form.handleSubmit}>
+    <div className="flex flex-col items-center mt-5">
+      <div>
+        <div className="bg-blue-100 inline-block p-8 text-3xl rounded-full">
+          <span role="img" aria-label="Avatar">
+            🥳
+          </span>
+        </div>
+      </div>
+      <form onSubmit={form.handleSubmit} className="mt-5 flex w-full">
         <input
-          className="block w-full p-1 border-gray-100 rounded-md border-2"
+          className="block px-1 w-full border-gray-100 rounded-md rounded-r-none border-r-0 border-2"
           type="text"
           name="username"
           placeholder="Jane Doe"
           value={form.values.username}
           onChange={form.handleChange}
         />
-        <button type="submit" className="mt-4 bg-green-500 block text-white px-4 py-2 rounded-md">
-          Sign In
+        <button
+          type="submit"
+          className="bg-green-500 block text-white rounded-md rounded-l-none py-2 px-4"
+        >
+          Login
         </button>
       </form>
     </div>
